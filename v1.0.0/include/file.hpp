4c578bd3c3acd61d6a4b0f818399a561a9806721
Core 2 Extreme <45873899+Core-2-Extreme@users.noreply.github.com>
2019-12-24 23:28:54 +0900

    Delete file.hpp

--- v1.0.0/include/file.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-int Share_save_to_file(std::string file_name, std::string data, std::string dir_path, bool delete_old_file, Handle fs_handle, FS_Archive fs_archive);
-
-std::string Share_load_from_file(std::string file_name, std::string dir_path, Handle fs_handle, FS_Archive fs_archive);
