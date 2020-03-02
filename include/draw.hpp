357849363d08ff229776e3c2aeec3c59186560d4
Core 2 Extreme <45873899+Core-2-Extreme@users.noreply.github.com>
2020-03-02 21:54:23 +0900

    v1.2.1

--- include/draw.hpp
+++ include/draw.hpp
@@ -4,15 +4,19 @@
 
 void Draw(std::string text, float x, float y, float text_size_x, float text_size_y, float r, float g, float b, float a);
 
-Result_with_string Draw_load_texture(std::string file_name, C2D_SpriteSheet sheet_texture, C2D_Image return_image[], int num_of_array);
+Result_with_string Draw_load_texture(std::string file_name, int sheet_map_num, C2D_Image return_image[], int start_num, int num_of_array);
 
-void Draw_texture(C2D_Image image[], int image_num, float x, float y, float x_size, float y_size);
+void Draw_texture(C2D_Image image[], C2D_ImageTint tint, int image_num, float x, float y, float x_size, float y_size);
 
 void Draw_with_specific_language(std::string text, int lang_num, float x, float y, float text_size_x, float text_size_y, float r, float g, float b, float a);
 
 void Draw_init(void);
 
-void Draw_load_system_fonts(void);
+void Draw_load_system_font(int system_font_num);
+
+void Draw_free_system_font(int system_font_num);
+
+void Draw_free_texture(int sheet_map_num);
 
 void Draw_exit(void);
 
--- include/setting_menu.hpp
+++ include/setting_menu.hpp
@@ -1,2 +1,8 @@
 
+void Setting_menu_init(void);
+
 void Setting_menu_main(void);
+
+void Setting_menu_update_check(void* arg);
+
+void Setting_menu_exit(void);
