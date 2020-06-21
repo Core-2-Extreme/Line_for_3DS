52b31112e22abeac2d8c78bbdec467f144e9d3e0
Core 2 Extreme <45873899+Core-2-Extreme@users.noreply.github.com>
2020-06-21 20:36:29 +0900

    v1.5.0

--- source/menu.cpp
+++ source/menu.cpp
@@ -128,7 +128,7 @@ void Menu_init(void)
 	menu_update_thread_run = true;
 	menu_check_connectivity_thread_run = true;
 	menu_update_thread = threadCreate(Menu_update_thread, (void*)(""), STACKSIZE, 0x18, -1, false);
-	//menu_check_connectivity_thread = threadCreate(Menu_check_connectivity_thread, (void*)(""), STACKSIZE, 0x30, -1, false);
+	menu_check_connectivity_thread = threadCreate(Menu_check_connectivity_thread, (void*)(""), STACKSIZE, 0x30, -1, false);
 
 	if (Sem_query_settings(SEM_ALLOW_SEND_APP_INFO))
 	{
