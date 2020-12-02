976512508926928d7a274210fa597888427d95ba
Core 2 Extreme <45873899+Core-2-Extreme@users.noreply.github.com>
2020-12-02 23:55:32 +0900

    v1.7.0

--- source/image_viewer.cpp
+++ source/image_viewer.cpp
@@ -63,7 +63,7 @@ std::string imv_img_load_thread_string = "Imv/Img load thread";
 std::string imv_img_dl_thread_string = "Imv/Img dl thread";
 std::string imv_init_string = "Imv/init";
 std::string imv_exit_string = "Imv/exit";
-std::string imv_ver = "v1.0.4";
+std::string imv_ver = "v1.0.5";
 Thread imv_parse_img_thread, imv_dl_img_thread, imv_load_img_thread;
 C2D_Image imv_c2d_image[64];
 
